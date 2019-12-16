1bd1c613a3fb6c7df1db1f79aed5613a2c7e1b60
Jenny Gebske <jennifer@gebske.de>
2019-12-16 13:48:39 +0100

    adjust filter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,11 +11,11 @@ import HourSelector from '../HourSelector/HourSelector'
 import ClusteredMarkers from '../Markers/ClusteredMarkers'
 import _ from './App.module.sass'
 
-const filterOptions = ['Alle', 'circ', 'lime', 'tier', 'voi']
+const filterOptions = ['Alle', 'Kabarett', 'Konzerte', 'Musical', 'Oper', 'Theater']
 
 function App (props) {
   const [markers, setMarkers] = useState([])
-  const [hour, setHour] = useState(18)
+  const [hour, setHour] = useState('2019-12-09T11:00:00.000Z')
   const [vendorFilter, setVendorFilter] = useState('Alle')
 
   useEffect(() => {
@@ -31,12 +31,15 @@ function App (props) {
   //  if the array is empty it runs only once otherwise it runs when depencies change
 
   const filteredByVendor = markers.filter(marker =>
-    vendorFilter === 'Alle' || marker.properties.v === vendorFilter)
-  const filteredByHour = filteredByVendor.filter(marker => marker.properties.h === hour)
+		vendorFilter === 'Alle' || marker.properties.type === vendorFilter)
+		
+  // const filteredByHour = filteredByVendor.filter(marker => marker.properties.date === hour)
+  const filteredByHour = filteredByVendor.filter(marker => marker)
 
+	
   const numberOfScootersByHour = useMemo(() =>
     filteredByVendor.reduce((array, marker) => {
-      const hour = marker.properties.h
+      const hour = marker.properties.date
       array[hour] = (array[hour] || 0) + 1
       return array
     }, [])
@@ -44,13 +47,15 @@ function App (props) {
   //  ⬆️ the histogram is only regenerated when the number of scooter change
   //  of course we could have two vendors with the same number of scooters but we don't
 
+	console.log('numberOfScootersByHour', numberOfScootersByHour)
+
   if (markers.length === 0) return null
 
   return <div className={_.app}>
     <div className={_.filter}>
-      <label htmlFor='vendorSelector'>Anbieter:</label>
+      <label htmlFor='vendorSelector'>Veranstaltungen:</label>
       <TabBar id='vendorSelector'
-        title='Anbieter auswählen'
+        title='Veranstaltung auswählen'
         selectedTab={vendorFilter}
         tabs={filterOptions}
         format={name => name.charAt(0).toUpperCase() + name.slice(1)}
@@ -73,16 +78,16 @@ function App (props) {
     </div>
     <div className={_.filter}>
       <div className={_.labelWrapper}>
-        <label className={_.timeLabel}>Fr. 05.07.2019 <strong>{hour}:00 Uhr</strong></label>
-        <label className={_.countLabel}>{numberOfScootersByHour[hour] || 0} Roller</label>
+        {/* <label className={_.timeLabel}>Dezember</label> */}
+        <label className={_.countLabel}>{numberOfScootersByHour[hour] || 0} Veranstaltung/en</label>
       </div>
       <HourSelector
         selectedHour={hour}
         onChange={setHour}
         histogramData={numberOfScootersByHour}
-        histogramMax={2833} />
+        histogramMax={34} />
       <label className={_.histogramLabel}>
-        Die Höhe der Balken zeigt die Anzahl der Roller zur jeweiligen Stunde.
+        Die Höhe der Balken zeigt die Anzahl der Veranstaltung am jeweiligen Tag.
       </label>
     </div>
   </div>
