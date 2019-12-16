61ae7b4abe6f96159f2b0653b76283b61d8e960a
Jenny Gebske <jennifer@gebske.de>
2019-12-16 13:50:36 +0100

    adjust histo

--- src/components/HourSelector/Histogram.js
+++ src/components/HourSelector/Histogram.js
@@ -4,9 +4,15 @@ import { darkGrey, red } from '../../shared/styles/colors.sass'
 import _ from './Histogram.module.sass'
 
 export default function Histogram (props) {
-  const { values, max, highlight, onClick } = props
+	const { values, max, highlight, onClick } = props
 
-  const normalizedValues = values.map(val => val / max)
+	console.log('props in histor', props)
+	
+	const values_only = Object.values(values)
+
+	const normalizedValues = values_only.map(val => val / max)
+	
+	console.log('normalizedValues', normalizedValues)
 
   const margin = 0.22
 
@@ -15,7 +21,7 @@ export default function Histogram (props) {
     viewBox={`0 0 ${values.length} 3.5`}
     width='100%'
     xmlns='http://www.w3.org/2000/svg'>
-    {normalizedValues.map((val, i) =>
+		{normalizedValues.map((val, i) =>
       <g key={i} onClick={() => onClick(i)}>
         <rect
           className={_.rect}
@@ -24,9 +30,9 @@ export default function Histogram (props) {
           width={1 - 2 * margin}
           height={val * 2.5}
           fill={i === highlight ? red : darkGrey} />
-        { i % 2 === 0 &&
-          <text className={_.text} x={i + 0.5} y='3.5' textAnchor='middle' fill={i === highlight ? red : darkGrey}>{i}</text>
-        }
+				{ i % 2 === 0 &&
+					<text className={_.text} x={i + 0.5} y='3.5' textAnchor='middle' fill={i === highlight ? red : darkGrey}>{i}</text>
+				}
       </g>
     )}
   </svg>
